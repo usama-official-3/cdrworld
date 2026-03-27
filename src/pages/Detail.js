7924929c14950a0ffbe2585a8b1b3cb6a5e8e47f
usama-official-3 <shusama100121@gmail.com>
2026-03-27 20:45:01 +0500

    update new

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -15,7 +15,6 @@ import Footer from "../components/Footer";
 import image1 from "../images/image1.jpg";
 import image2 from "../images/image2.jpg";
 import image3 from "../images/image3.jpg";
-import { Helmet } from "react-helmet-async";
 import SEO from "../components/SEO";
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import { Link, useSearchParams, useNavigate } from "react-router-dom";
 import { Star } from "lucide-react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Helmet } from "react-helmet-async";
 import SEO from "../components/SEO";
 
 const FILES_PER_PAGE = 12;
